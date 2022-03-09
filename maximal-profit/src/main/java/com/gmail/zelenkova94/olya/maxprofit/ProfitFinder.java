f22e9faa12f391e85442bdea177cae53c4c177c3
olga-pisakova <olya.zelenkova94@gmail.com>
2022-03-09 11:48:36 +0100

    JavaDoc added.

--- maximal-profit/src/main/java/com/gmail/zelenkova94/olya/maxprofit/ProfitFinder.java
+++ maximal-profit/src/main/java/com/gmail/zelenkova94/olya/maxprofit/ProfitFinder.java
@@ -1,16 +1,21 @@
 package com.gmail.zelenkova94.olya.maxprofit;
 
+/**
+ * JavaDoc.
+ */
 public class ProfitFinder {
-    public int findMaxProfit(int[] array) {
-        if(array == null)
+    public int findMaxProfit(final int[] array) {
+        if (array == null) {
             throw new IllegalArgumentException("Given array must not be null");
+        }
         int maxProfit = 0;
         int currentProfit;
-        for(int i = 1; i < array.length; i++) {
-            for(int j = 0; j < i; j++) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < i; j++) {
                 currentProfit = array[i] - array[j];
-                if(currentProfit > maxProfit)
+                if (currentProfit > maxProfit) {
                     maxProfit = currentProfit;
+                }
             }
         }
         return maxProfit;
