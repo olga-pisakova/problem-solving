c6a8edbe54b7c6e600e62a392b268804c6abe933
olga-pisakova <olya.zelenkova94@gmail.com>
2022-03-10 22:49:41 +0100

    Created generator of URL.

--- min-perimeter-rectangle/src/test/java/com/gmail/zelenkova94/olya/minperimeter/MinPerimeterFinderTest.java
+++ min-perimeter-rectangle/src/test/java/com/gmail/zelenkova94/olya/minperimeter/MinPerimeterFinderTest.java
@@ -3,8 +3,6 @@ package com.gmail.zelenkova94.olya.minperimeter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MinPerimeterFinderTest {
 
     @Test
